b77e3292258ce98b84d379d264801dd0451ffca6
Subbramanian Lakshmanan <sxl146830@utdallas.edu>
2015-03-27 10:03:57 -0500

    Added Chat rooms
    Working on chat rooms

--- appchallenge/router/main.js
+++ appchallenge/router/main.js
@@ -2,13 +2,23 @@ module.exports=function(app)
 {
 	app.get('/',function(req,res){
 		console.log('server is running');
-		res.redirect('/home');
+		res.redirect('/index');
+	});
+
+	app.get('/index', function(req, res){
+		res.render('index.html');
 	});
 	
-	app.get('/home', function(req, res){
+	app.get('/home/?', function(req, res){
 		//Render dynamic files
+		console.log(req.query.key);
+		app.locals.roomID = req.query.key;
 		res.render('home.html');
 		//render static files using sendFile
 		//res.sendFile('./static/home.html');
+	});
+
+	app.get('/rooms', function(req, res){
+		res.render('rooms.html');
 	});
 }
--- appchallenge/server.js
+++ appchallenge/server.js
@@ -5,6 +5,7 @@ var cookieParser = require("cookie-parser");
 var session  = require('express-session');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+//var $ = require('jquery')(require("jsdom").jsdom().parentWindow);;
 
 require('./router/main')(app);
 
@@ -22,24 +23,89 @@ var oneDay = 86400000;
 app.use(express.static(__dirname + '/static',{maxAge: oneDay}));
 
 
-var server=http.listen(8080,function(){
+var server = http.listen(8080,function(){
 	console.log("Server listening on port 8080");
 });
+
+function User(data){
+    this.username = data;
+    this.userMailID = data+"@google.com";
+    this.available = 'available';
+
+    this.getUserInfo =  function(){
+        return "userName: " + this.username + " emailID:" + this.userMailID + " available: " + this.available;
+    };
+
+}
+
+function Room(roomName){
+    this.users = new Array();
+    this.count = 0;
+    this.roomName = roomName;
+
+    this.addUser = function(data){
+        console.log(data);
+        this.users.push(data);
+        this.count++;
+    };
+}
+
+var usersList = new Array();
+var roomsList = new Array();
 
 io.on('connection', function(socket){
     console.log('user is connected at: '+ socket.id);
 
-        // socket.on('chat message', function(msg){
-        //         console.log('Message:'+ msg);
-        //         if(msg!=""){
-        //                 io.emit('chat message', msg);
-        //         }else{
-        //                 io.emit('error', "Empty message");
-        //         }
-        // });
+        var status;
 
         socket.on('addUser', function(username){
-        	console.log("User:"+ username);
+            console.log('user:'+ username);
+            var user = new User(username);
+            usersList.push(user);
+            console.log(user.getUserInfo());
+            // add the client's username to the global list
+            socket.currentUser = user;
+            // echo to client they've connected
+            socket.emit('update', username + ' has connected');
+            // echo globally (all clients) that a person has connected
+            socket.broadcast.emit('statusUpdate', username + ' has connected');
+            // update the list of users in chat, client-side
+            socket.emit('updateUsers', JSON.stringify(user));
+        });
+
+        socket.on('getAllRooms', function(){
+            console.log("Returning all rooms details");
+
+            if(roomsList.length==0){
+                status = false;
+                console.log("No rooms available");
+            }else{
+                status = true;
+            }
+            socket.emit('updateRooms', status, JSON.stringify(roomsList));
+            socket.broadcast.emit('updateRooms', status, JSON.stringify(roomsList));    
+        });
+
+        socket.on('addRoom', function(roomName){
+            console.log("Room: "+ roomName);
+            var room = new Room(roomName);
+            console.log(room);
+            room.addUser(socket.currentUser);
+            roomsList.push(room);
+            status = true;  // room created
+            socket.emit('updateRooms', status, JSON.stringify(roomsList));
+            socket.broadcast.emit('updateRooms', status, JSON.stringify(roomsList));
+
+            socket.emit('update', roomName + ' has been created');
+        });
+
+        socket.on('getRoomDetails', function(){
+            console.log("Getting Room Details");
+            $(roomsList).each(function(key, value) {
+                if(app.locals.roomID == key){
+                    console.log("found room details");
+                }
+            });
         });
 
         socket.on('disconnect', function(){
